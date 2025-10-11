49b5b37e399a42a0917604f1973dfacfc3c8b885
Pau <pauburguetvela@gmail.com>
2025-10-11 18:41:00 +0200

    feat: Implemented Iterator for EntryVector

--- src/utils.rs
+++ src/utils.rs
@@ -42,6 +42,14 @@ impl<T> EntryVector<T> {
     pub fn is_empty(&self) -> bool {
         self.vector.is_empty()
     }
+
+    #[inline]
+    pub fn iter(&self) -> EntryVectorIter<'_, T> {
+        EntryVectorIter {
+            index: 0,
+            vector: self,
+        }
+    }
 }
 
 impl<T> Index<usize> for EntryVector<T> {
@@ -53,6 +61,25 @@ impl<T> Index<usize> for EntryVector<T> {
             "Out of bounds access"
         );
         &self.vector[internal_index..(internal_index + self.entry_size)]
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct EntryVectorIter<'a, T> {
+    index: usize,
+    vector: &'a EntryVector<T>,
+}
+
+impl<'a, T> Iterator for EntryVectorIter<'a, T> {
+    type Item = &'a [T];
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.vector.len() {
+            let n = &self.vector[self.index];
+            self.index += 1;
+            Some(n)
+        } else {
+            None
+        }
     }
 }
 
@@ -83,5 +110,19 @@ mod tests {
         let entry = [1, 2, 3, 4, 5];
         v.push(entry);
         let _entry = &v[1];
+    }
+
+    #[test]
+    fn test_iter() {
+        let entries = [[1usize, 2, 3], [4, 5, 9], [22, 33, 1]];
+        let mut v = EntryVector::<usize>::new(3);
+
+        entries.iter().for_each(|e| v.push(e.iter().copied()));
+
+        v.iter().zip(entries).for_each(|(value, expected)| {
+            assert_eq!(expected, value);
+        });
+
+        assert_eq!(v.iter().count(), entries.len());
     }
 }
